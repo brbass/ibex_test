53ed626c5703ee5c93dd03aa3441b26bad4a8e61
Brody Bassett <brbass@umich.edu>
2018-02-28 13:05:57 -0500

    Updated files.

--- vera_p1e/openmc/multigroup_benchmark/view.py
+++ vera_p1e/openmc/multigroup_benchmark/view.py
@@ -12,12 +12,13 @@ sp = openmc.StatePoint("statepoint.200.h5")
 
 # K-eff results
 keff = sp.k_combined
+np.savetxt("k_eigenvalue.txt", keff)
 print(keff)
 
 # Tally results
-tally = sp.get_tally(name='flux')
-print(tally)
-flux = tally.get_slice(scores=['flux-Y0,0'])
-print(flux)
-print(len(flux.mean))
+# tally = sp.get_tally(name='flux')
+# print(tally)
+# flux = tally.get_slice(scores=['flux-Y0,0'])
+# print(flux)
+# print(len(flux.mean))
 
